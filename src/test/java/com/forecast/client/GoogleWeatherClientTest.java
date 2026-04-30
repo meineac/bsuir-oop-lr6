e0d095f87d86d0df0f52a5d9e5b4a0e4556f7e0b
Matsvei Prakapovich <matveyprokopovech@gmail.com>
2026-05-01 00:30:29 +0300

    test: add forecast retrieval test for googleweatherclient

--- src/test/java/com/forecast/client/GoogleWeatherClientTest.java
+++ src/test/java/com/forecast/client/GoogleWeatherClientTest.java
@@ -1,5 +1,6 @@
 package com.forecast.client;
 
+import com.forecast.model.ForecastWeather;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
@@ -9,6 +10,7 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -93,6 +95,44 @@ public class GoogleWeatherClientTest {
                 client.getCurrentTemperature(new BigDecimal("53.9006"), new BigDecimal("27.5590")));
 
         assertEquals("failed to decode response: missing temperature data", exception.getMessage());
+        mockServer.verify();
+    }
+
+    @Test
+    void getForecast_Success() {
+        String jsonResponse = """
+                {
+                    "forecastDays": [
+                        {
+                            "displayDate": {
+                                "year": 2026,
+                                "month": 4,
+                                "day": 30
+                            },
+                            "maxTemperature": {
+                                "degrees": 10.1,
+                                "unit": "CELSIUS"
+                            },
+                            "minTemperature": {
+                                "degrees": 2,
+                                "unit": "CELSIUS"
+                            }
+                        }
+                    ]
+                }
+                """;
+
+        mockServer.expect(requestTo("http://mock-googleweather.com/currentConditions:lookup?" +
+                "key=test-api-key&location.latitude=53.9006&location.longitude=27.5590"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(jsonResponse, MediaType.APPLICATION_JSON));
+
+        ForecastWeather result = client.getForecast(new BigDecimal("53.9006"), new BigDecimal("27.5590"));
+
+        assertEquals(1, result.getDays().size());
+        assertEquals(new BigDecimal("2"), result.getDays().getFirst().getMinTemperature());
+        assertEquals(new BigDecimal("10.1"), result.getDays().getFirst().getMaxTemperature());
+        assertEquals(LocalDate.of(2026, 4, 30), result.getDays().getFirst().getDate());
         mockServer.verify();
     }
 }
